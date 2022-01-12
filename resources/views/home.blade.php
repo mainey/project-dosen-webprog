a7784844ca4b12b6186b09899453266242b9f1d5
Michael Benedict <michaelbt@live.com>
2022-01-12 19:54:17 +0700

    home page banner image WIP

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,25 @@
 
 @section('content')
 
-<h3>Home Page Placeholder</h3>
+<div class="bg-image home-image blur-image dim-image"></div>
+
+<div class="home-title-text title-text-above">Here's the best way to</div>
+<div class="home-title-text title-text">Find your perfect Home</div>
+
+<div class="col-md-10" id="content">
+    p
+    <br>
+    p
+    <br>
+    p
+    <br>
+    p
+    <br>
+    p
+    <br>
+    p
+    <br>
+</div>
 
 @endsection
 
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -13,6 +13,8 @@
     {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous"> --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet" type="text/css" >
+
     <style>
         body {
             font-family: 'Nunito', sans-serif;
@@ -20,7 +22,7 @@
     </style>
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #68BBE3">
+    <nav class="navbar navbar-expand-lg navbar-light sticky-top" style="background-color: #68BBE3">
         <div class="container-fluid col-md-10">
           <a class="navbar-brand" href="#"><b>DRE</b></a>
           <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
@@ -28,9 +30,8 @@
           </button>
           <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-              <a class="nav-link" href="#">Placeholder</a>
-              <a class="nav-link" href="#">Placeholder</a>
+              <a class="nav-link" aria-current="page" href="#">Home</a>
+              <a class="nav-link" href="#">Listing</a>
             </div>
           </div>
         </div>
@@ -39,9 +40,7 @@
     <div class="container-fluid">
         <div class="row">
             <div class="col-md-1"></div>
-            <div class="col-md-10">
-                @yield('content')
-            </div>
+            @yield('content')
             <div class="col-md-1"></div>
         </div>
     </div>
@@ -49,25 +48,7 @@
 
     <footer class="fixed-bottom" style="background-color: #68BBE3">
       <div class="p-3 ml-auto">
-        <h2>DRE</h2>
-      </div>
-      <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #68BBE3">
-        <div class="container-fluid">
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-start" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
-              <a class="nav-link" href="#">Placeholder</a>
-              <a class="nav-link" href="#">Placeholder</a>
-            </div>
-          </div>
-        </div>
-      </nav>
-      
-      <div class="p-3 ml-auto">
-        ©2021 
+        <h2>DRE ©2021</h2>
       </div>
     </footer>
 
