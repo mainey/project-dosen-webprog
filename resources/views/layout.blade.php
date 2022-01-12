91b1c1d36fe17d4ea618bdcbc5431bc4081de205
Michael Benedict <michaelbt@live.com>
2022-01-12 20:14:38 +0700

    navbar transparent buat gambar

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -22,20 +22,24 @@
     </style>
 </head>
 <body>
+    @if(Request::path() == '/')
+    <nav class="navbar navbar-expand-lg navbar-light fixed-top navbar-expand-md">
+    @else
     <nav class="navbar navbar-expand-lg navbar-light sticky-top" style="background-color: #68BBE3">
+    @endif
         <div class="container-fluid col-md-10">
-          <a class="navbar-brand" href="#"><b>DRE</b></a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-              <a class="nav-link" aria-current="page" href="#">Home</a>
-              <a class="nav-link" href="#">Listing</a>
+            <a class="navbar-brand text-light" href="#"><b>DRE</b></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-end" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                <a class="nav-link text-light" aria-current="page" href="#">Home</a>
+                <a class="nav-link text-light" href="#">Listing</a>
+                </div>
             </div>
-          </div>
         </div>
-      </nav>
+    </nav>
 
     <div class="container-fluid">
         <div class="row">
