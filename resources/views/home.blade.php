2fbce65c6ca9f0a040143aba4e4deb03eeced543
Michael Benedict <michaelbt@live.com>
2022-01-12 21:16:44 +0700

    blue backrgound 0.4 & background for search bar

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -9,12 +9,14 @@
 
 
 
-<div class="home-title-text title-text-above">Here's the best way to</div>
-<div class="home-title-text title-text">Find your perfect Home</div>
-<form class="home-title-text title-search d-flex">
-    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success" type="submit">Search</button>
-  </form>
+<div class="home-title title-text-above">Here's the best way to</div>
+<div class="home-title title-text">Find your perfect Home</div>
+<div class="home-title title-search">
+    <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search for your dream home" aria-label="Search">
+        <button class="btn bg-light" type="submit">Search</button>
+    </form>
+</div>
 
 <div class="col-md-10" id="content">
     p
