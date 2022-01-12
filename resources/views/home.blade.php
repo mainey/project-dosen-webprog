00336f988c40ef4f05484311732c2700c4b777f1
Garrrrth <66118224+Garrrrth@users.noreply.github.com>
2022-01-12 20:57:15 +0700

    search bar + unblur

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,10 +4,17 @@
 
 @section('content')
 
-<div class="bg-image home-image blur-image dim-image"></div>
+
+    <div class="bg-image home-image dim-image img-edge"></div>
+
+
 
 <div class="home-title-text title-text-above">Here's the best way to</div>
 <div class="home-title-text title-text">Find your perfect Home</div>
+<form class="home-title-text title-search d-flex">
+    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+    <button class="btn btn-outline-success" type="submit">Search</button>
+  </form>
 
 <div class="col-md-10" id="content">
     p
