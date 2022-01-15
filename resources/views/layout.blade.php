bb8d107656a1328556ed26881ff3665891ef2d31
Michael Benedict <michaelbt@live.com>
2022-01-15 22:59:15 +0700

    footer margin top

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -52,7 +52,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 <footer>
-    <div class="text-center text-light" style="background-color: #68BBE3; padding: 10px">
+    <div class="text-center text-light" style="background-color: #68BBE3; padding: 10px; margin-top: 20px">
       Copyright © 2021 DRE
     </div>
 </footer>
