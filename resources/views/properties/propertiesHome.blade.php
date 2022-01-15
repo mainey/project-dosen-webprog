916954464f3633c7cc587ff421f5b882b785c8f0
Michael Benedict <michaelbt@live.com>
2022-01-15 15:11:44 +0700

    Properties layout blade

---
+++ resources/views/properties/propertiesHome.blade.php
@@ -0,0 +1,13 @@
+@extends('layout')
+
+@section('title', 'Properties')
+
+@section('content')
+
+<div class="col-md-10 mx-auto" id="content">
+    <div class="container">
+    </div>
+
+</div>
+
+@endsection
---
+++ resources/views/properties/propertiesView.blade.php
@@ -0,0 +1,12 @@
+@extends('layout')
+
+@section('title', 'Properties')
+
+@section('content')
+
+<div class="col-md-10 mx-auto" id="content">
+    <div class="container">
+    </div>
+</div>
+
+@endsection
