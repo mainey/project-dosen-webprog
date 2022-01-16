5df46b1fb1d1b142cfc140337fafaa974dea7af4
Veliny <mulia.darma1@gmail.com>
2022-01-16 22:36:53 +0700

    Login Controller

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class LoginController extends Controller
@@ -12,5 +13,26 @@ class LoginController extends Controller
 
     public function registerUser(){
         return view('register');
+    }
+
+    public function register(Request $request){
+        $user = new User();
+        $user->name = $request->user;
+        $user->email = $request->email;
+        $user->address = $request->address;
+        $user->password = bcrypt($request->password);
+        $user->phone = $request->phone;
+        $user->role = 'member';
+    }
+
+    public function login(Request $request){
+        $email = $request->email;
+        $password = $request->password;
+
+        if(auth()->attempt(['email'=>$email, 'password'=>$password])){
+            return redirect('/home');
+        }else{
+            return redirect('/login');
+        }
     }
 }
