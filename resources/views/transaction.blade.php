26ac78e7ad35e08cd76e65c65ec02f6388682a21
Nak332 <59206901+Nak332@users.noreply.github.com>
2022-01-18 03:11:17 +0700

    Update transaction.blade.php

--- resources/views/transaction.blade.php
+++ resources/views/transaction.blade.php
@@ -3,8 +3,150 @@
 @section('title', 'Transaction')
 
 @section('content')
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
 
-<div class="container">
+<div class="page-content container">
+    <div class="page-header text-blue-d2">
+     
+
+        
+
+    <div class="container px-0">
+        <div class="row mt-4">
+            <div class="col-12 col-lg-12">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="text-center text-150">
+                            Invoice ID: #111-222
+                        </div>
+                    </div>
+                </div>
+                <!-- .row -->
+
+                <hr class="row brc-default-l1 mx-n1 mb-4" />
+
+                <div class="row">
+                    <div class="col-sm-6">
+                        <div>
+                            <span class="text-sm text-grey-m2 align-middle">To:</span>
+                            <span class="text-600 text-110 text-blue align-middle">Alex Doe</span>
+                        </div>
+                        <div class="text-grey-m2">
+                            <div class="my-1">
+                                Street, City
+                            </div>
+                            <div class="my-1">
+                                State, Country
+                            </div>
+                            <div class="my-1"><i class="fa fa-phone fa-flip-horizontal text-secondary"></i> <b class="text-600">111-111-111</b></div>
+                        </div>
+                    </div>
+                    <!-- /.col -->
+
+                    
+                    <!-- /.col -->
+                </div>
+
+                <div class="mt-4">
+                    <div class="row text-600 text-secondary-d3 bgc-default-tp1 py-25">
+                        <div class="d-none d-sm-block col-1">#</div>
+                        <div class="col-9 col-sm-5">Description</div>
+                        <div class="d-none d-sm-block col-4 col-sm-2">Duration</div>
+                        <div class="d-none d-sm-block col-sm-2">Unit Price</div>
+                        <div class="col-2">Amount</div>
+                    </div>
+                    <hr>
+
+                 
+
+                        <div class="row mb-2 mb-sm-0 py-25 bgc-default-l4">
+                            <div class="d-none d-sm-block col-1">1</div>
+                            <div class="col-9 col-sm-5">Greenlake</div>
+                            <div class="d-none d-sm-block col-2">1 Year</div>
+                            <div class="d-none d-sm-block col-2 text-95">$500</div>
+                            <div class="col-2 text-secondary-d2">$500</div>
+                        </div>
+                    </div>
+
+                    <div class="row border-b-2 brc-default-l2"></div>
+
+                    <!-- or use a table instead -->
+                    <!--
+            <div class="table-responsive">
+                <table class="table table-striped table-borderless border-0 border-b-2 brc-default-l1">
+                    <thead class="bg-none bgc-default-tp1">
+                        <tr class="text-white">
+                            <th class="opacity-2">#</th>
+                            <th>Description</th>
+                            <th>Qty</th>
+                            <th>Unit Price</th>
+                            <th width="140">Amount</th>
+                        </tr>
+                    </thead>
+
+                    <tbody class="text-95 text-secondary-d3">
+                        <tr></tr>
+                        <tr>
+                            <td>1</td>
+                            <td>Domain registration</td>
+                            <td>2</td>
+                            <td class="text-95">$10</td>
+                            <td class="text-secondary-d2">$20</td>
+                        </tr> 
+                    </tbody>
+                </table>
+            </div>
+            -->
+
+                    <div class="row mt-3">
+                        <div class="col-12 col-sm-7 text-grey-d2 text-95 mt-2 mt-lg-0">
+                            
+                        </div>
+
+                        <div class="col-12 col-sm-5 text-grey text-90 order-first order-sm-last">
+                            <div class="row my-2">
+                                <div class="col-7 text-right ">
+                                  SubTotal
+                                </div>
+                                <div class="col-5">
+                                    <span class="text-120 text-secondary-d1">$2,250</span>
+                                </div>
+                            </div>
+
+                            <div class="row my-2">
+                                <div class="col-7 text-right">
+                                    Tax (10%)
+                                </div>
+                                <div class="col-5">
+                                    <span class="text-110 text-secondary-d1">$225</span>
+                                </div>
+                            </div>
+
+                            <hr>
+
+                            <div class="row my-2 align-items-center bgc-primary-l3 ">
+                                <div class="col-7 text-right">
+                                    Total Amount
+                                </div>
+                                <div class="col-5">
+                                    <span class="text-150 text-success-d3 opacity-2">$2,475</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <hr />
+
+                    <div>
+                        <span class="text-secondary-d1 text-105">Thank you for your business</span>
+                        <a href="#" class="btn btn-info btn-bold px-4 float-right mt-3 mt-lg-0">Pay Now</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+{{-- <div class="container">
     <div class="row justify-content-center pt-3">
     
      <div class="col-8 border">
@@ -47,7 +189,7 @@
         </table>
      </div>
     </div>
-</div>
+</div> --}}
 
     </div>
     </div>
